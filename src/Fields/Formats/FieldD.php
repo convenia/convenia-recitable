5e897ad0a8969054c54083e891e3e0f10e30df4e
anderson <anderson.forta@hotmail.com>
2018-03-15 15:11:58 -0300

    add date field type

---
+++ src/Fields/Formats/FieldD.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Convenia\PayrollFileReader\Fields\Formats;
+
+class FieldD extends FieldBase
+{
+    /**
+     * Return the formatted field.
+     *
+     * @param string $case
+     * @return string
+     */
+    public function format($case = 'title')
+    {
+        $this->stringCase($case);
+
+        $actualLength = $this->value->length();
+        $this->value = $this->value->truncate($this->getLength());
+
+        if ($actualLength < $this->getLength()) {
+            $this->value = $this->value->padRight($this->getLength());
+        }
+
+        return $this->value;
+    }
+
+    /**
+     * @param $case
+     */
+    public function stringCase($case): void
+    {
+        if (strcmp($case, 'title')) {
+            $this->value = $this->value->toTitleCase();
+            return;
+        }
+        if (strcmp($case, 'upper')) {
+            $this->value = $this->value->toUpperCase();
+            return;
+        }
+    }
+}
